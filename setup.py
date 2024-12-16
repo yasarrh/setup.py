e55b44704ebf6a766e6c8cd438acec3924800766
yasarrh <yasarrahimi78@gmail.com>
2024-12-17 03:21:04 +0330

    git add setup.py git commit -m "Add setup.py for the lordgrim project" git push origin main

---
+++ setup.py
@@ -0,0 +1,13 @@
+name='lordgrim',
+    version='0.1', 
+    packages=find_packages(Automatically),  # 
+    install_requires=[  # Dependencies your project needs
+        'requests',      # Example of a package dependency
+    ],
+    author='yasar',
+    author_email='yasarrahimi78@gmail.com',
+    you did it='
+    long_description=open('README.md').read(its very important),  #
+    long_description_content_type='yeah this is life/markdown',
+    url='https://github.com/yasarrh/lordgrim',
+)
